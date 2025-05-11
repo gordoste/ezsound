eac74457a6632c41de0ee76c9c8363084da1a21e
Stephen Gordon <gordoste@iinet.net.au>
2025-05-12 09:09:28 +1000

    runtests.sh: Add support for multiple tests, help message

--- runtests.sh
+++ runtests.sh
@@ -1,43 +1,76 @@
 #!/bin/bash
 
-cardname="ezsound6x8"
+default_cardname="ezsound6x8"
 
-while getopts "d:" opt; do
+usage() {
+	cat - <<EOF
+$0: Run a variety of tests for the ezsound card
+
+Usage: $0 [-h] [-c <cardname>] [-C <capturedev>] [-P <playbackdev>] testname ...
+
+	-h:	Print help (this message)
+	-c:	Specify the card name to use (default is $default_cardname)
+	-C:	Specify ALSA device for capture
+	-P:	Specify ALSA device for playback
+
+Available tests:
+	recplay:	Record 10 seconds of stereo audio and then play it back
+EOF
+
+	exit 0
+}
+
+cardname=$default_cardname
+
+while getopts "hc:C:P:" opt; do
 	case "$opt" in
-		d)	opt_d="yes"; device="$OPTARG";;
+		h)	usage;;
 		c)	cardname="$OPTARG";;
+		C)	opt_capdev="$OPTARG";;
+		P)	opt_pbdev="$OPTARG";;
 	esac
 done
 
 pbdevice="plughw:$cardname,1"
 capdevice="plughw:$cardname,0"
 
-if [ "$opt_d" = "yes" ]; then
-	pbdevice=$device
-	capdevice=$device
+if [ ! -z "$opt_capdev" ]; then
+	capdevice=$opt_capdev
+fi
+
+if [ ! -z "$opt_pbdev" ]; then
+	pbdevice=$opt_pbdev
 fi
 
 echo "Using '$capdevice' for capture"
 echo "Using '$pbdevice' for playback"
 
-read -p "Press enter to start recording for 10 seconds"
+do_recplay_test() {
+	read -p "Press enter to start recording for 10 seconds"
 
-arecord -Vstereo -d10 -D$capdevice -r96000 -c2 -fS32_LE sample.wav
+	arecord -Vstereo -d10 -D$capdevice -r96000 -c2 -fS32_LE sample.wav
 
-if [ "$?" -ne 0 ]; then
-	echo "Recording error... aborting"
-	exit 1
-fi
-echo
+	if [ "$?" -ne 0 ]; then
+		echo "Recording error... aborting"
+		exit 1
+	fi
+	echo
 
-read -p "Press enter to play back recorded file"
+	read -p "Press enter to play back recorded file"
 
-aplay -Vstereo -D$pbdevice -r96000 -c2 -fS32_LE sample.wav
+	aplay -Vstereo -D$pbdevice -r96000 -c2 -fS32_LE sample.wav
 
-if [ "$?" -ne 0 ]; then
-	echo "Playback error... aborting"
-	exit 2
-fi
-echo
+	if [ "$?" -ne 0 ]; then
+		echo "Playback error... aborting"
+		exit 2
+	fi
+	echo
+}
+
+for arg; do
+	if [ "$arg" = "recplay" ]; then
+		do_recplay_test
+	fi
+done
 
 exit 0
